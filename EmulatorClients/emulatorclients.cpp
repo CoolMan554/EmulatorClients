ade04eaeff19be215ea11a1d3c2a0304e5b7d453
KonychevDmitry <dmib00@mail.ru>
2024-05-30 19:48:04 +0500

    Инициализация основных полей класса EmulatorClients

--- EmulatorClients/emulatorclients.cpp
+++ EmulatorClients/emulatorclients.cpp
@@ -3,5 +3,8 @@
 EmulatorClients::EmulatorClients(QString address, int port, int countConnection, int period, QObject *parent)
     : QObject{parent}
 {
-
+    m_address = address;
+    m_port = port;
+    m_countConnection = countConnection;
+    m_period = period;
 }
